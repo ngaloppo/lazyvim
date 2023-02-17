beff9c3e219b7407b33dd54e54f7b03638e53fde
Nico Galoppo <nico.galoppo@intel.com>
2023-02-16 21:56:06 -0800

    Add cmp-tmux support

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -8,8 +8,18 @@ return {
       { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>" },
     },
     config = true,
-    -- cond = function()
-    --   return vim.env.TMUX ~= nil
-    -- end,
+    cond = function()
+      return vim.env.TMUX ~= nil
+    end,
+  },
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "andersevenrud/cmp-tmux" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "tmux" } }))
+    end,
   },
 }
