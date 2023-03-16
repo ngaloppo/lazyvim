4e1217933b7bb74af5ddb614a2390d9ea9adaa19
Nico Galoppo <nico.galoppo@intel.com>
2023-03-16 14:22:08 -0700

    python formatters/lang servers, surround keymaps

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,46 @@
 return {
+  -- set colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
+    },
+  },
+  -- add null-ls formatters
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.formatting.black, -- python formatter
+        nls.builtins.formatting.isort, -- sort python imports
+      })
+    end,
+  },
+  -- customize surround keymappings
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  },
+  -- add default servers to lspconfig (automatically installed with mason)
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pyright = {},
+      },
     },
   },
 }
